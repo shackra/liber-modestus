146fca701311d3b2590168d0ab3e3068ff08bbf3
Jorge Javier Araya Navarro <jorge@esavara.cr>
2025-08-21 01:07:21 -0600

    feat(backend): map dates to files in Tempora folder
    This covers the times of:
    
    - Advent
    - Pasch
    - Septuagesima to Quinquagesima
    - Lent
    - Epiphany
    - Pentecost

--- backend/src/tempus/calc.py
+++ backend/src/tempus/calc.py
@@ -2,6 +2,7 @@ import datetime
 import pdb
 from typing import Optional
 
+from dateutil import easter
 from dateutil.relativedelta import relativedelta
 
 
@@ -35,3 +36,85 @@ def get_tempora_for_advent(now: datetime.datetime) -> Optional[str]:
 
     # `Adv` matches what's on src/divinum-officium/web/www/missa/<lang>/Tempora
     return f"Adv{sunday_of_advent}-{day_of_week_after_advent}"
+
+
+def get_tempora_for_epiphany(now: datetime.datetime) -> Optional[str]:
+    epiphany = datetime.datetime(now.year, 1, 6)
+    if now.date() < epiphany.date():
+        return None
+
+    # if today is the Epiphany of the Lord
+    if now.date() == epiphany.date():
+        return f"{now.month:02}-{now.day:02}"
+
+    # Sundays after Epiphany
+    days_until_first_sunday = 7 - epiphany.isoweekday()
+    first_sunday_after_epiphany = epiphany + relativedelta(days=days_until_first_sunday)
+    days_after_first_sunday = (now - first_sunday_after_epiphany).days
+
+    # Beyond the Fifth Sunday after Epiphany
+    if days_after_first_sunday > (5 * 7):
+        return None
+
+    if now.date() == first_sunday_after_epiphany.date():
+        return "Epi1-0"
+
+    sunday_n_after_epiphany = (days_after_first_sunday // 7) + 1
+
+    return f"Epi{sunday_n_after_epiphany}-{now.isoweekday() if now.isoweekday() != 7 else 0}"
+
+
+def get_tempora_for_pentecost(now: datetime.datetime) -> Optional[str]:
+    empty_tomb = easter.easter(now.year, easter.EASTER_WESTERN)
+    first_sunday_of_pentecost = empty_tomb + relativedelta(days=49)
+
+    if now.date() < first_sunday_of_pentecost:
+        return None
+
+    days_after_first_sunday = (now.date() - first_sunday_of_pentecost).days
+    sundays_after_pentecost = days_after_first_sunday // 7
+
+    return f"Pent{sundays_after_pentecost:02}-{now.isoweekday() if now.isoweekday() != 7 else 0}"
+
+
+def get_tempora_for_pasch(now: datetime.datetime) -> Optional[str]:
+    empty_tomb = easter.easter(now.year, easter.EASTER_WESTERN)
+    days_after_resurrection_sunday = (now.date() - empty_tomb).days
+
+    if now.date() < empty_tomb or days_after_resurrection_sunday > ((8 * 7) - 1):
+        return None
+
+    sundays_after_resurrection_sunday = days_after_resurrection_sunday // 7
+
+    return f"Pasc{sundays_after_resurrection_sunday}-{now.isoweekday() if now.isoweekday() != 7 else 0}"
+
+
+def get_tempora_for_lent(now: datetime.datetime) -> Optional[str]:
+    first_sunday_of_lent = easter.easter(
+        now.year, easter.EASTER_WESTERN
+    ) - relativedelta(days=42)
+
+    if now.date() < first_sunday_of_lent:
+        return None
+
+    days_after_first_sunday = (now.date() - first_sunday_of_lent).days
+    sundays_after_first_sunday_of_lent = (days_after_first_sunday // 7) + 1
+
+    if sundays_after_first_sunday_of_lent > 6:
+        return None  # the 7th Sunday is Easter!
+
+    return f"Quad{sundays_after_first_sunday_of_lent}-{now.isoweekday() if now.isoweekday() != 7 else 0}"
+
+
+def get_tempora_for_quadp(now: datetime.datetime) -> Optional[str]:
+    septuagesima_sunday = easter.easter(
+        now.year, easter.EASTER_WESTERN
+    ) - relativedelta(days=63)
+    days_after_septuagesima_sunday = (now.date() - septuagesima_sunday).days
+
+    if now.date() < septuagesima_sunday or days_after_septuagesima_sunday > (3 * 7 - 1):
+        return None
+
+    sundays_after_septuagesima_sunday = (days_after_septuagesima_sunday // 7) + 1
+
+    return f"Quadp{sundays_after_septuagesima_sunday}-{now.isoweekday() if now.isoweekday() != 7 else 0}"
---
+++ backend/tests/test_tempus.py
@@ -0,0 +1,290 @@
+from datetime import datetime
+
+from tempus import (
+    get_tempora_for_advent,
+    get_tempora_for_epiphany,
+    get_tempora_for_lent,
+    get_tempora_for_pasch,
+    get_tempora_for_pentecost,
+    get_tempora_for_quadp,
+)
+
+
+def test_tempora_adv_first_sunday():
+    expected = "Adv1-0"
+    now = datetime(2025, 11, 30)
+    result = get_tempora_for_advent(now)
+
+    assert expected == result
+
+
+def test_tempora_adv_first_sunday_monday():
+    expected = "Adv1-1"
+    now = datetime(2025, 12, 1)
+    result = get_tempora_for_advent(now)
+
+    assert expected == result
+
+
+def test_tempora_adv_first_sunday_thursday():
+    expected = "Adv1-4"
+    now = datetime(2025, 12, 4)
+    result = get_tempora_for_advent(now)
+
+    assert expected == result
+
+
+def test_tempora_adv_second_sunday():
+    expected = "Adv2-0"
+    now = datetime(2025, 12, 7)
+    result = get_tempora_for_advent(now)
+
+    assert expected == result
+
+
+def test_tempora_adv_second_sunday_monday():
+    expected = "Adv2-1"
+    now = datetime(2025, 12, 8)
+    result = get_tempora_for_advent(now)
+
+    assert expected == result
+
+
+def test_tempora_adv_forth_sunday_tuesday():
+    expected = "Adv4-2"
+    now = datetime(2025, 12, 23)
+    result = get_tempora_for_advent(now)
+
+    assert expected == result
+
+
+def test_tempora_adv_2022_forth_sunday_friday():
+    expected = "Adv4-5"
+    now = datetime(2022, 12, 23)
+    result = get_tempora_for_advent(now)
+
+    assert expected == result
+
+
+def test_tempora_adv_2022_forth_sunday_saturday():
+    expected = None
+    now = datetime(2022, 12, 24)
+    result = get_tempora_for_advent(now)
+
+    assert expected == result
+
+
+def test_tempora_epi_before_day():
+    expected = None
+    now = datetime(2025, 1, 5)
+
+    result = get_tempora_for_epiphany(now)
+
+    assert expected == result
+
+
+def test_tempora_epi_today_day():
+    expected = "01-06"
+    now = datetime(2025, 1, 6, 19, 55, 16)
+
+    result = get_tempora_for_epiphany(now)
+
+    assert expected == result
+
+
+def test_tempora_epi_first_sunday():
+    expected = "Epi1-0"
+    now = datetime(2025, 1, 12)
+
+    result = get_tempora_for_epiphany(now)
+
+    assert expected == result
+
+
+def test_tempora_epi_second_sunday():
+    expected = "Epi2-0"
+    now = datetime(2025, 1, 19)
+
+    result = get_tempora_for_epiphany(now)
+
+    assert expected == result
+
+
+def test_tempora_epi_third_sunday():
+    expected = "Epi3-0"
+    now = datetime(2025, 1, 26)
+
+    result = get_tempora_for_epiphany(now)
+
+    assert expected == result
+
+
+def test_tempora_epi_forth_sunday():
+    expected = "Epi4-0"
+    now = datetime(2025, 2, 2)
+
+    result = get_tempora_for_epiphany(now)
+
+    assert expected == result
+
+
+def test_tempora_epi_fifth_sunday():
+    expected = "Epi5-0"
+    now = datetime(2025, 2, 9)
+
+    result = get_tempora_for_epiphany(now)
+
+    assert expected == result
+
+
+def test_tempora_pent_first_sunday():
+    expected = "Pent01-0"
+    now = datetime(2025, 6, 15)
+
+    result = get_tempora_for_pentecost(now)
+
+    assert expected == result
+
+
+def test_tempora_pent_first_sunday_feria_2():
+    expected = "Pent01-1"
+    now = datetime(2025, 6, 16)
+
+    result = get_tempora_for_pentecost(now)
+
+    assert expected == result
+
+
+def test_tempora_pent_sunday_23():
+    expected = "Pent23-0"
+    now = datetime(2025, 11, 16)
+
+    result = get_tempora_for_pentecost(now)
+
+    assert expected == result
+
+
+def test_tempora_pent_sunday_23_feria_2():
+    expected = "Pent23-1"
+    now = datetime(2025, 11, 17)
+
+    result = get_tempora_for_pentecost(now)
+
+    assert expected == result
+
+
+def test_tempora_pent_sunday_24():
+    expected = "Pent24-0"
+    now = datetime(2025, 11, 23)
+
+    result = get_tempora_for_pentecost(now)
+
+    assert expected == result
+
+
+def test_tempora_pent_sunday_24_feria_2():
+    expected = "Pent24-1"
+    now = datetime(2025, 11, 24)
+
+    result = get_tempora_for_pentecost(now)
+
+    assert expected == result
+
+
+def test_tempora_pasc():
+    expected = "Pasc0-0"
+    now = datetime(2025, 4, 20)
+
+    result = get_tempora_for_pasch(now)
+
+    assert expected == result
+
+
+def test_tempora_pasc_second_sunday():
+    expected = "Pasc1-0"
+    now = datetime(2025, 4, 27)
+
+    result = get_tempora_for_pasch(now)
+
+    assert expected == result
+
+
+def test_tempora_pasc_third_sunday():
+    expected = "Pasc2-0"
+    now = datetime(2025, 5, 4)
+
+    result = get_tempora_for_pasch(now)
+
+    assert expected == result
+
+
+def test_tempora_pasc_fifth_sunday():
+    expected = "Pasc5-0"
+    now = datetime(2025, 5, 25)
+
+    result = get_tempora_for_pasch(now)
+
+    assert expected == result
+
+
+def test_tempora_pasc_sixth_sunday():
+    expected = "Pasc6-0"
+    now = datetime(2025, 6, 1)
+
+    result = get_tempora_for_pasch(now)
+
+    assert expected == result
+
+
+def test_tempora_pent():
+    expected = "Pasc7-0"
+    now = datetime(2025, 6, 8)
+
+    result = get_tempora_for_pasch(now)
+
+    assert expected == result
+
+
+def test_tempora_first_sunday_of_lent():
+    expected = "Quad1-0"
+    now = datetime(2025, 3, 9)
+
+    result = get_tempora_for_lent(now)
+
+    assert expected == result
+
+
+def test_tempora_lent_palm_sunday():
+    expected = "Quad6-0"
+    now = datetime(2025, 4, 13)
+
+    result = get_tempora_for_lent(now)
+
+    assert expected == result
+
+
+def test_tempora_lent_pasc_none():
+    expected = None
+    now = datetime(2025, 4, 20)
+
+    result = get_tempora_for_lent(now)
+
+    assert expected == result
+
+
+def test_tempora_quadp_sunday():
+    expected = "Quadp1-0"
+    now = datetime(2025, 2, 16)
+
+    result = get_tempora_for_quadp(now)
+
+    assert expected == result
+
+
+def test_tempora_quadp_sunday_3_saturday_after_ash_wednesday():
+    expected = "Quadp3-6"
+    now = datetime(2025, 3, 8)
+
+    result = get_tempora_for_quadp(now)
+
+    assert expected == result
