57a530434129ff1291bbf62ef903197f069bc649
Jorge Javier Araya Navarro <jorge@esavara.cr>
2025-08-21 10:21:09 -0600

    feat(backend): implement tokenizer for texts of divinum officium

---
+++ backend/src/captator/parser/tokenizer.py
@@ -0,0 +1,82 @@
+import re
+from typing import List, Tuple
+
+Token = Tuple[str, str]  # (kind, value)
+
+
+class Tokenizer:
+    def __init__(self, text: str):
+        self.text = text
+        self.tokens: List[Token] = []
+
+    def tokenize(self):
+        token_specification = [
+            ("LPAREN", r"\("),
+            ("RPAREN", r"\)"),
+            ("LBRACK", r"\["),
+            ("RBRACK", r"\]"),
+            ("SEMICOLON", r";"),
+            ("COLON", r":"),
+            ("DOLLAR", r"\$"),
+            ("BANG", r"!"),
+            ("AMP", r"&"),
+            ("EQUAL", r"="),
+            ("SLASH", r"/"),
+            ("DASH", r"-"),
+            ("NUMBER", r"\d+(\.\d+)?"),
+            ("WORD", r"[^\s;\[\]()$!&=:/-]+"),
+            ("SKIP", r"[ \t]+"),
+            ("NEWLINE", r"\n"),
+        ]
+        tok_regex = "|".join(
+            f"(?P<{name}>{pattern})" for name, pattern in token_specification
+        )
+
+        raw_tokens = []
+        for mo in re.finditer(tok_regex, self.text):
+            kind = mo.lastgroup
+            value = mo.group()
+            if kind == "SKIP" or kind is None:
+                continue
+
+            raw_tokens.append((kind, value))
+
+        i = 0
+        header = False
+        header_text = []
+        bang = False
+        while i < len(raw_tokens):
+            match raw_tokens[i][0]:
+                case "SEMICOLON":
+                    if i + 1 < len(raw_tokens) and raw_tokens[i + 1][0] == "SEMICOLON":
+                        self.tokens.append(("SEP", ";;"))
+                        i += 1
+                    else:
+                        self.tokens.append(raw_tokens[i])
+
+                case "LBRACK":
+                    header = True
+                case "RBRACK":
+                    header = False
+                    self.tokens.append(("HEADER", f"[{" ".join(header_text)}]"))
+                    header_text = []
+                case "BANG":
+                    bang = True
+                    self.tokens.append(raw_tokens[i])
+                case "NEWLINE":
+                    bang = False
+                case _:
+                    if header:
+                        header_text.append(raw_tokens[i][1])
+                    else:
+                        self.tokens.append(raw_tokens[i])
+            i += 1
+
+    def __iter__(self):
+        return iter(self.tokens)
+
+    def __len__(self):
+        return len(self.tokens)
+
+    def __repr__(self):
+        return " ".join([f"{name} {value}" for name, value in self.tokens])
---
+++ backend/tests/test_captator_parser_tokenizer.py
@@ -0,0 +1,71 @@
+from captator import parser
+
+
+def test_tokenize_text_1():
+    text = """
+    [Rank]
+Secunda die infra Octavam Epiphaniæ;;Semiduplex;;5.6;;ex Sancti/01-06
+    """
+    tokenized = parser.Tokenizer(text)
+    tokenized.tokenize()
+
+    assert len(tokenized) == 17
+
+
+def test_tokenize_text_2():
+    text = """
+    [Rule]
+Gloria
+CredoDA
+Prefatio=Epi
+Suffragium=Maria2;Papa;Ecclesia;;
+Infra octavam Epiphaniæ Domini
+    """
+    tokenized = parser.Tokenizer(text)
+    tokenized.tokenize()
+
+    assert len(tokenized) == 18
+
+
+def test_tokenize_text_3():
+    text = """
+    [Introitus]
+!Malach 3:1; 1 Par 29:12
+v. Ecce, advénit dominátor Dóminus: et regnum in manu ejus et potéstas et impérium.
+!Ps 71:1
+Deus, judícium tuum Regi da: et justítiam tuam Fílio Regis.
+&Gloria
+v. Ecce, advénit dominátor Dóminus: et regnum in manu ejus et potéstas et impérium.
+    """
+    tokenized = parser.Tokenizer(text)
+    tokenized.tokenize()
+
+    assert len(tokenized) == 60
+
+
+def test_tokenize_text_4():
+    text = """
+    [Evangelium]
+@Tempora/Nat2-0
+
+[Offertorium]
+@Tempora/Nat30
+
+[Secreta]
+@Commune/C2
+
+[Commemoratio Secreta] (rubrica tridentina)
+!Pro S. Stephano Protomartyre
+@Sancti/08-03:Secreta
+!Pro S. Joanne Evangelista
+@Sancti/12-27:Secreta
+!Pro Ss. Innocentibus
+@Sancti/12-28:Secreta
+
+[Communio]
+@Tempora/Nat30
+    """
+    tokenized = parser.Tokenizer(text)
+    tokenized.tokenize()
+
+    assert len(tokenized) == 58
